8940e84474f5f625f58ef568720a984a4254b496
norki <norkink89@mail.ru>
2022-12-23 12:50:57 +0400

    Compelet

--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,27 @@ public class Main {
         return arr;
     }
     public static void task1() {
-        int[ ] arr = generateRandomArray();
+        int[] arr = generateRandomArray();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
+            System.out.println(arr[i]);
                     }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
-        int min = arr[1];
+        int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
            if (arr[i]<min) {
                 min = arr[i];
             }
             }
-               int max = arr[1];
+               int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i]>max) {
                 max = arr[i];
             }
         }
-        System.out.println("Минимальная сумма трат за день составила " + max + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
-        float average = (float) sum/30f;
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
+        double average = sum/30.0;
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
     }
     public static void task4() {
